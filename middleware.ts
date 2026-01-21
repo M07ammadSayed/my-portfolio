2acf29029439729b036025ba881622be36d29e1e
Muhammad Sayyid <msayed.ms2005@gmail.com>
2026-01-21 22:15:52 +0200

    fix(pwa): absolute fix for CSP blocking using inline attributes

--- middleware.ts
+++ middleware.ts
@@ -15,6 +15,13 @@ export function middleware(request: NextRequest) {
 		return NextResponse.next();
 	}
 
+	if (pathname.includes("offline.html")) {
+		const response = NextResponse.next();
+		response.headers.delete("Content-Security-Policy");
+		response.headers.delete("X-Frame-Options");
+		return response;
+	}
+
 	const nonce = btoa(
 		String.fromCharCode(...crypto.getRandomValues(new Uint8Array(16))),
 	);
