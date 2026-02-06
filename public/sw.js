1e5c89084f7c4846c4d8e93c684dd36bb49e4786
Muhammad Sayyid <msayed.ms2005@gmail.com>
2026-02-06 18:17:51 +0200

    fix(sw): block analytics and insights requests in offline mode to clean console

--- public/sw.js
+++ public/sw.js
@@ -1,63 +1,4 @@
-// const CACHE_NAME = "muhammad-portfolio-v99999";
-// const OFFLINE_URL = "/offline";
-
-// const PRECACHE_ASSETS = [
-// 	OFFLINE_URL,
-// 	"/manifest.json",
-// 	"/favicon.ico",
-// 	"/icon.svg",
-// ];
-
-// self.addEventListener("install", (event) => {
-// 	event.waitUntil(
-// 		caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_ASSETS)),
-// 	);
-// 	self.skipWaiting();
-// });
-
-// self.addEventListener("activate", (event) => {
-// 	event.waitUntil(
-// 		caches
-// 			.keys()
-// 			.then((keys) =>
-// 				Promise.all(
-// 					keys
-// 						.filter((key) => key !== CACHE_NAME)
-// 						.map((key) => caches.delete(key)),
-// 				),
-// 			),
-// 	);
-// 	self.clients.claim();
-// });
-
-// self.addEventListener("fetch", (event) => {
-// 	const { request } = event;
-// 	const url = new URL(request.url);
-
-// 	if (!url.protocol.startsWith("http")) return;
-
-// 	if (request.mode === "navigate") {
-// 		event.respondWith(
-// 			fetch(request)
-// 				.then((networkResponse) => {
-// 					return networkResponse;
-// 				})
-// 				.catch(async () => {
-// 					const cache = await caches.open(CACHE_NAME);
-// 					return await cache.match(OFFLINE_URL);
-// 				}),
-// 		);
-// 		return;
-// 	}
-
-// 	event.respondWith(
-// 		caches.match(request).then((cachedResponse) => {
-// 			return cachedResponse || fetch(request);
-// 		}),
-// 	);
-// });
-
-const CACHE_NAME = "my-portfolio-v41357";
+const CACHE_NAME = "my-portfolio-v312034";
 const OFFLINE_URL = "/offline";
 
 const PRECACHE_ASSETS = [
@@ -91,14 +32,26 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
 	const { request } = event;
+	const url = new URL(request.url);
+
+	if (
+		url.hostname.includes("google-analytics.com") ||
+		url.hostname.includes("analytics.google.com") ||
+		url.pathname.includes("_vercel/insights") ||
+		url.pathname.includes("/vitals") ||
+		request.method !== "GET"
+	) {
+		if (!navigator.onLine) {
+			event.respondWith(new Response(null, { status: 204 }));
+			return;
+		}
+	}
 
 	if (!request.url.startsWith("http")) return;
 
 	if (request.mode === "navigate") {
 		event.respondWith(
-			fetch(request).catch(() => {
-				return caches.match(OFFLINE_URL);
-			}),
+			fetch(request).catch(() => caches.match(OFFLINE_URL)),
 		);
 		return;
 	}
@@ -107,9 +60,9 @@ self.addEventListener("fetch", (event) => {
 		caches.match(request).then((cachedResponse) => {
 			if (cachedResponse) return cachedResponse;
 
-			return fetch(request).catch(
-				() => new Response(null, { status: 404 }),
-			);
+			return fetch(request).catch(() => {
+				return new Response(null, { status: 404 });
+			});
 		}),
 	);
 });
